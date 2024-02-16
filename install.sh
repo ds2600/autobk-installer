fbf54f54db77f7d149d95ee0835857f21692731b
ds2600 <bill@ishiganto.com>
2024-02-16 06:24:08 -0500

    First rewrite

--- install.sh
+++ install.sh
@@ -20,35 +20,7 @@ ABR_GET_URL="https://github.com/ds2600/autobk-r/archive/refs/tags/latest.zip"
 GUI_GET_URL="https://github.com/ds2600/autobk-controller/archive/refs/tags/latest.zip"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CFG_LOCATION="www/inc/config.php"
-DB_ROOT_P=$( echo $RANDOM | md5sum | head -c 24 )
-
-# configure() is used to enter .env variables into the AutoBk-GUI configuration file
-configure() {
-   local ARG_FIX=$( echo "$3" | sed 's/\//\\\//g' )
-   if [ $1 == 'autobk' ]; then
-     sed -i 's/{'"$2"'}/'"$ARG_FIX"'/' ${SCRIPT_LOCATION}autobk/autobk.ini >> $SCRIPT_DIR/install.log 2>&1
-   else
-     sed -i 's/{'"$2"'}/'"$ARG_FIX"'/' $GUI_LOCATION$CFG_LOCATION >> $SCRIPT_DIR/install.log 2>&1
-   fi
-}
-
-# spin() is used to display the stupid dots during the installation process
-spin() {
-   pid=$! # Get PID of background command
-   while kill -0 $pid > /dev/null 2>&1  # Signal 0 just tests whether the process exists
-    do
-     echo -n "."
-     sleep 1 
-   done
-}
-
-# autobk-install() is used to install various dependencies using apt
-autobk-install() {
-	/usr/bin/apt install -y $1 >> $SCRIPT_DIR/install.log 2>&1 &
-	spin
-}
-
+DB_ROOT_PWD=$( echo $RANDOM | md5sum | head -c 24 )
 
 # Execute as superuser preserving env variables
 if [ $EUID != 0 ]; then
@@ -63,43 +35,36 @@ else
     echo "You forgot the .env file!"; exit;
 fi
 
-mkdir $SCRIPT_LOCATION
+# autobk-install() is used to install various dependencies using apt
+autobk-install() {
+	/usr/bin/apt install -y $1 >> $SCRIPT_DIR/install.log 2>&1 &
+}
 
 echo -e "############### " && date >> $SCRIPT_DIR/install.log 2>&1
+
+# Make the core AutoBk directory
+mkdir -p $AUTOBK_DIR
 
 # Install required packages
 echo -e "Updating Apt packages"
 /usr/bin/apt update -y >> $SCRIPT_DIR/install.log 2>&1 &
-spin
 
-echo -e "\nInstalling Apache2 web server"
-autobk-install apache2
-
-echo -e "\nInstalling PHP & Requirements" 
-autobk-install libapache2-mod-php7.4
-autobk-install php7.4
-autobk-install php7.4-common
-autobk-install php7.4-curl
-autobk-install php7.4-dev
-autobk-install php7.4-gd
-autobk-install php-pear
-autobk-install php7.4-mysql
-autobk-install php7.4-ldap
-
-echo -e "\nInstalling Python & Requirements"
+echo -e "\nInstalling required apt packages"
+autobk-install unzip
 autobk-install python3
 autobk-install python3-pip
-# Install Python dependencies
-pip3 install beautifulsoup4 >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-pip3 install easysnmp >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-pip3 install mysql-connector-python >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-pip3 install pyOpenSSL >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-pip3 install simplejson >> $SCRIPT_DIR/install.log 2>&1 &
-spin
+autobk-install nginx
+
+
+echo -e "\nInstalling nvm and Node.js"
+# Install NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+# Source nvm script to ensure it's available
+. ~/.nvm/nvm.sh
+
+# Install latest Node.js and npm
+nvm install node
 
 # Install MariaDB server unless specified in .env
 if [ $INSTALL_DB == "yes" ]; then
@@ -108,132 +73,99 @@ if [ $INSTALL_DB == "yes" ]; then
 
 	echo -e "\nConfiguring MariaDB"
 	# Set root password, delete anonymous users, delete remote root, delete test database, flush privileges
-	mysql -sfu root -Bse "UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_P}') WHERE User='root';DELETE FROM mysql.user WHERE User='';DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');DROP DATABASE IF EXISTS test;DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';FLUSH PRIVILEGES;" >> $SCRIPT_DIR/install.log 2>&1 &
+	mysql -sfu root -Bse "UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PWD}') WHERE User='root';DELETE FROM mysql.user WHERE User='';DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');DROP DATABASE IF EXISTS test;DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';FLUSH PRIVILEGES;" >> $SCRIPT_DIR/install.log 2>&1 &
 	spin
 	
 	# Create user as designated in the .env file
-	mysql -u root -p$DB_ROOT_P -Bse "CREATE USER '${SQL_UN}'@localhost IDENTIFIED BY '${SQL_PASS}';GRANT ALL PRIVILEGES ON *.* TO '${SQL_UN}'@localhost IDENTIFIED BY '${SQL_PASS}';FLUSH PRIVILEGES;" >> $SCRIPT_DIR/install.log 2>&1 &
+	mysql -u root -p$DB_ROOT_PWD -Bse "CREATE USER '${SQL_UN}'@localhost IDENTIFIED BY '${SQL_PASS}';GRANT ALL PRIVILEGES ON *.* TO '${SQL_UN}'@localhost IDENTIFIED BY '${SQL_PASS}';FLUSH PRIVILEGES;" >> $SCRIPT_DIR/install.log 2>&1 &
 	spin
 
 	# Create the AutoBk Database
 	echo "CREATE DATABASE AutoBk;" | mysql -u $SQL_UN -p$SQL_PASS
-	
-	# Get the latest AutoBK SQL and load it
-	wget --no-cache $SQL_GET_URL >> $SCRIPT_DIR/install.log 2>&1 &
-	spin
-	mysql -u $SQL_UN -p$SQL_PASS AutoBk < $SCRIPT_DIR/autobk-installer.sql &
-	spin
-
 
 #		-- grant AutoBk user privileges to JUST the AutoBk Database - disabled for now
 #		-- GRANT ALL PRIVILEGES ON '${AUTOBK_DB}'.* TO '${SQL_UN}'@localhost;
 else
-	echo -e "\nSkipping MariaDB installation per user"
+	echo -e "\nSkipping MariaDB installation per configuration"
 fi
-	
-echo -e "\nInstalling Unzip"
-autobk-install unzip
+
+# Get newest version of AutoBk Controller
+echo -e "\nSetting up AutoBk Controller"
+wget --no-cache $GUI_GET_URL >> $SCRIPT_DIR/install.log 2>&1 &
+unzip latest.zip -d ${AUTOBK_DIR}controller/ >> $SCRIPT_DIR/install.log 2>&1 &
+cd ${AUTOBK_DIR}controller/autobk-controller/
+npm install >> $SCRIPT_DIR/install.log 2>&1 &
+
+# Generate JWT secret
+echo -e "\nGenerating JWT secret"
+JWT_SECRET=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
+
+# Build the AutoBk Controller .env file
+echo -e "\nBuilding AutoBk Controller .env file"
+cp ${AUTOBK_DIR}controller/autobk-controller/.env.example ${AUTOBK_DIR}controller/autobk-controller/.env
+sed -i 's/DB_DATABASE=AutoBk/DB_DATABASE='"$AUTOBK_DB"'/g' ${AUTOBK_DIR}controller/autobk-controller/.env
+sed -i 's/DB_USER=/DB_USER='"$SQL_UN"'/g' ${AUTOBK_DIR}controller/autobk-controller/.env
+sed -i 's/DB_PASSWORD=/DB_PASSWORD='"$SQL_PASS"'/g' ${AUTOBK_DIR}controller/autobk-controller/.env
+sed -i 's/DB_HOST=/DB_HOST='"$SQL_HOST"'/g' ${AUTOBK_DIR}controller/autobk-controller/.env
+sed -i 's/JWT_SECRET=/JWT_SECRET='"$JWT_SECRET"'/g' ${AUTOBK_DIR}controller/autobk-controller/.env
+
+# Run the database migrations
+echo -e "\nMigrating database"
+npx sequelize-cli db:migrate >> $SCRIPT_DIR/install.log 2>&1 &
+
+# Build AutoBk Controller
+echo -e "\nBuilding AutoBk Controller"
+npm run build >> $SCRIPT_DIR/install.log 2>&1 &
+
+# Configure Nginx
+GUI_LOCATION=${AUTOBK_DIR}controller/autobk-controller/build
+
+echo -e "\nConfiguring Nginx"
+NGINX_BLOCK="server {
+    listen 80;
+    server_name $GUI_DOMAIN;
+    location / {
+      root $GUI_LOCATION;
+      index index.html;
+    }
+}"
+
+echo "$NGINX_BLOCK" > /etc/nginx/sites-available/autobk-controller
+ln -s /etc/nginx/sites-available/autobk-controller /etc/nginx/sites-enabled/autobk-controller
+rm /etc/nginx/sites-enabled/default
+nginx -t >> $SCRIPT_DIR/install.log 2>&1
+systemctl restart nginx >> $SCRIPT_DIR/install.log 2>&1
+
+# Start AutoBk Controller API
+echo -e "\nStarting AutoBk Controller"
+npm run start-api >> $SCRIPT_DIR/install.log 2>&1 &
 
 # Get newest version of AutoBk and unzip it
 echo -e "\nSetting up AutoBk"
-wget --no-cache $AB_GET_URL >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-unzip autobk.zip -d ${SCRIPT_LOCATION}autobk/ >> $SCRIPT_DIR/install.log 2>&1 &
-spin
+wget --no-cache $ABP_GET_URL >> $SCRIPT_DIR/install.log 2>&1 &
+unzip autobk.zip -d ${AUTOBK_DIR}python/ >> $SCRIPT_DIR/install.log 2>&1 &
 
-# Modify AutoBk .ini to match .env
-configure autobk 'dir' $SCRIPT_LOCATION
-configure autobk 'user' $SQL_UN
-configure autobk 'pwd' $SQL_PASS
-configure autobk 'host' $SQL_HOST
+# Create a virtual environment for Python
+echo -e "\nCreating Python virtual environment"
+python3 -m venv $AUTOBK_DIR/python/venv >> $SCRIPT_DIR/install.log 2>&1 &
+# Install required PIP packages
+echo -e "\nInstalling required PIP packages"
+source $AUTOBK_DIR/venv/bin/activate
+pip3 install beautifulsoup4 >> $SCRIPT_DIR/install.log 2>&1 &
+pip3 install easysnmp >> $SCRIPT_DIR/install.log 2>&1 &
+pip3 install mysql-connector-python >> $SCRIPT_DIR/install.log 2>&1 &
+pip3 install pyOpenSSL >> $SCRIPT_DIR/install.log 2>&1 &
+pip3 install simplejson >> $SCRIPT_DIR/install.log 2>&1 &
 
-# Make AutoBk a systemd service
-echo -e "\nEnabling AutoBk as a service"
+# Run AutoBk Python
+echo -e "\nRunning AutoBk-P"
+python3 ${AUTOBK_DIR}python/autobk-p/srvc_autobk.py >> $SCRIPT_DIR/install.log 2>&1 &
 
-# Create and save the autobk.service file
-touch /etc/systemd/system/autobk.service
-echo '[Unit]' >> /etc/systemd/system/autobk.service
-echo 'Description=AutoBk' >> /etc/systemd/system/autobk.service
-echo 'After=multi-user.target' >> /etc/systemd/system/autobk.service
-echo '[Service]' >> /etc/systemd/system/autobk.service
-echo 'Type=idle' >> /etc/systemd/system/autobk.service
-echo 'User=root' >> /etc/systemd/system/autobk.service
-echo 'WorkingDirectory='"$SCRIPT_LOCATION"'autobk' >> /etc/systemd/system/autobk.service
-echo 'ExecStart=/usr/bin/python3.8 '"$SCRIPT_LOCATION"'autobk/srvc_autobk.py' >> /etc/systemd/system/autobk.service
-echo '[Install]' >> /etc/systemd/system/autobk.service
-echo 'WantedBy=multi-user.target' >> /etc/systemd/system/autobk.service
-
-# Reload systemd and enable autobk.service
-systemctl daemon-reload >> $SCRIPT_DIR/install.log 2>&1
-sleep 5
-systemctl enable autobk.service >> $SCRIPT_DIR/install.log 2>&1
-sleep 5
-echo -e "Starting AutoBk"
-systemctl start autobk.service >> $SCRIPT_DIR/install.log 2>&1
-
-# Get newest version of AutoBk-GUI and unzip it
-echo -e "Setting up AutoBk web interface"
-wget --no-cache $GUI_GET_URL >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-unzip cabgui.zip -d $GUI_LOCATION >> $SCRIPT_DIR/install.log 2>&1 &
-spin
-
-# Copy example GUI configuration file
-cp ${GUI_LOCATION}www/inc/config.example.php $GUI_LOCATION$CFG_LOCATION 
-
-# Modify GUI configuration file to match .env
-configure gui 'timezone' $TIME_ZONE
-configure gui 'guilocation' $GUI_LOCATION
-configure gui 'sqlun' $SQL_UN
-configure gui 'sqlpass' $SQL_PASS
-configure gui 'sqlhost' $SQL_HOST
-configure gui 'sqlport' $SQL_PORT
-configure gui 'sqlchar' $SQL_CHAR
-configure gui 'ablocation' $AB_LOCATION
-configure gui 'smtphost' $SMTP_HOST
-configure gui 'smtpfrom' $SMTP_FROM
-configure gui 'smtpemail' $SMTP_EMAIL
-
-# Disable the default virtual host
-a2dissite 000-default >> $SCRIPT_DIR/install.log 2>&1
-
-# Add new directory permissions and WWW directory to virtual conf
-# Will probably eventually create designated AutoBk virtual host
-GUI_NOSLASH=$( echo "$GUI_LOCATION" | sed 's/\//\\\//g' )
-sed -i '1i<Directory '"$GUI_NOSLASH"'>' /etc/apache2/sites-available/000-default.conf
-sed -i '2iOptions Indexes FollowSymLinks' /etc/apache2/sites-available/000-default.conf
-sed -i '3iAllowOverride None' /etc/apache2/sites-available/000-default.conf
-sed -i '4iRequire all granted' /etc/apache2/sites-available/000-default.conf
-sed -i '5i<\/Directory> ' /etc/apache2/sites-available/000-default.conf
-sed -i 's/\/var\/www\/html/'"$GUI_NOSLASH"'www/' /etc/apache2/sites-available/000-default.conf > $SCRIPT_DIR/install.log 2>&1
-chown -R www-data:www-data $GUI_LOCATION
-chmod -R 755 $GUI_LOCATION
-a2ensite 000-default >> $SCRIPT_DIR/install.log 2>&1
-
-echo -e "\nRestarting Apache"
-systemctl reload apache2 >> $SCRIPT_DIR/install.log 2>&1
-
-echo -e "\nAdding command line aliases"
-echo "source ${GUI_LOCATION}/autobk.bash" >> /etc/bash.bashrc
-source /etc/bash.bashrc
-
-echo -e "\nModifying MOTD"
-chmod -x /etc/update-motd.d/*
-ln -s ${GUI_LOCATION}/00-autobk /etc/update-motd.d/00-autobk
-chmod +x /etc/update-motd.d/00-autobk
-
-
-echo -e "Remove downloaded files"
-rm -f $SCRIPT_DIR/autobk-installer.sql
-rm -f $SCRIPT_DIR/autobk.zip
-rm -f $SCRIPT_DIR/cabgui.zip
 
 echo -e "Installation complete"
 
 # Display database root password 
 if [ $INSTALL_DB == "yes" ]; then
-	echo -e "Your database 'root' password, you should save this as it won't be visible again:\n ${DB_ROOT_P}"
+	echo -e "Your database 'root' password, you should save this as it won't be visible again:\n ${DB_ROOT_PWD}"
 fi
-
-echo -e "\n Please run 'sudo systemctl restart autobk.service'."
 
